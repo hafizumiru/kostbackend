a8b38770bc19630aaf8293044266d0c8c4ed5167
hafizumiru <miruruhatsune@gmail.com>
2025-07-07 15:55:07 +0700

    docs: melakukan update dan commit pertama dan dipush ke github

---
+++ src/test/java/com/kostapp/kostbackend/KostbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kostapp.kostbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KostbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
